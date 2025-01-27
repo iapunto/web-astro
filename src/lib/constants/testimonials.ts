729aac3b6fa58e6c3f78b59eba92178a18e28ab2
Desarrollo IA Punto <desarrollo@iapunto.com>
2025-01-27 15:15:44 -0500

    Add Testimonial and 404 Page

--- src/lib/constants/testimonials.ts
+++ src/lib/constants/testimonials.ts
@@ -4,26 +4,51 @@ export const testimonials: Testimonial[] = [
   {
     id: 1,
     quote:
-      "We've looked at a lot of SEO solutions but their guys were always the clear favorite. They have the right strategy and they've been awesome to work with.",
-    author: "Hine Warner",
-    position: "SEO Manager",
-    company: "TechCorp",
+      "Trabajar con IA Punto ha sido una experiencia verdaderamente revolucionaria para Distribuidora Uvayo. Su enfoque único en el marketing digital, específicamente a través de SMMA (Social Media Marketing Agency), ha llevado nuestra presencia en línea a nuevas alturas.",
+    author: "Ubilerma Meneses",
+    position: "CEO Manager",
+    company: "Distribuidora UVAYO",
+    image: "https://iapunto.com/wp-content/uploads/2023/01/tes003.jpg",
   },
   {
     id: 2,
     quote:
-      "The team's attention to detail and commitment to delivering exceptional results has significantly improved our online presence.",
-    author: "Sarah Johnson",
-    position: "Marketing Director",
-    company: "InnovateX",
+      "Como CEO de Integral Health, me complace compartir la extraordinaria experiencia que hemos tenido trabajando con IA Punto en la creación de nuestra página web. En el competitivo mundo de la atención dental, la presencia en línea se ha vuelto esencial, y gracias al equipo de IA Punto, hemos dado un salto digital que ha transformado nuestra clínica odontológica.",
+    author: "Yaneth Zamora",
+    position: "CEO Manager",
+    company: "Integral Health",
+    image: "https://iapunto.com/wp-content/uploads/2023/12/ICON.png",
   },
   {
     id: 3,
     quote:
-      "Their innovative approach and deep understanding of digital marketing has helped us achieve our business goals faster than expected.",
-    author: "Michael Chen",
-    position: "CEO",
-    company: "GrowthLabs",
+      "El equipo de IA Punto ha demostrado ser excepcional en su capacidad para comprender nuestras necesidades y objetivos específicos. Su enfoque centrado en el cliente y la atención meticulosa a los detalles han sido clave para el éxito de nuestras campañas digitales.",
+    author: "Pedro Zambrano",
+    position: "Biogénica Global - Director",
+    company: "Biogénica Global",
+    image:
+      "https://iapunto.com/wp-content/uploads/2023/12/82f43e79-81f3-4872-949f-01b2e051e9f1.jpeg",
+  },
+
+  {
+    id: 4,
+    quote:
+      "Como autora, necesitaba una plataforma online que no solo presentara mi libro 'Yo Soy Poder', sino que también conectara con mi audiencia de una manera significativa. IA Punto entendió a la perfección mi visión y creó un sitio web que transmite la esencia de mi trabajo. Gracias a su estrategia de marketing digital, he podido llegar a un público mucho más amplio y difundir un mensaje tan importante como la prevención del bullying. Estoy inmensamente agradecida por su profesionalismo y dedicación.",
+    author: "Magnolia García",
+    position: "Yo Soy Poder - Autora",
+    company: "Yo Soy Poder",
+    image:
+      "https://inedif.com.co/congreso-internacional-de-cerebro-y-educacion/wp-content/uploads/2024/11/Imagen-de-WhatsApp-2024-02-12-a-las-12.09.33_304ee209.jpg",
+  },
+
+  {
+    id: 5,
+    quote:
+      "En INEDIF, buscábamos un socio estratégico que nos ayudara a posicionarnos como líderes en el ámbito del neuroliderazgo y la economía digital. IA Punto superó nuestras expectativas. No solo desarrollaron una página web moderna y funcional, sino que también gestionaron el marketing integral y la producción audiovisual de dos de nuestros congresos más importantes. Su enfoque innovador y su capacidad para generar contenido de alto impacto han sido fundamentales para el éxito de nuestros eventos y el crecimiento de nuestra comunidad. Los recomiendo ampliamente.",
+    author: "Carlos Monnery Caiaffa",
+    position: "INEDIF - Director",
+    company: "INEDIF",
+    image: "https://inedif.com.co/wp-content/uploads/2023/09/carlos-2.png",
   },
   // Más testimonios aquí
 ];
