3e978e6f4fa2bd841974f85ff65250055df66a49
skailan-saas <skailan.saas@gmail.com>
2025-07-16 18:41:21 -0500

    Astro middleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import type { MiddlewareHandler } from 'astro';
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  const response = await next();
+  response.headers.set(
+    'Content-Security-Policy',
+    [
+      "default-src 'self'",
+      "script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net 'unsafe-inline'",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+      "img-src 'self' data: https: https://res.cloudinary.com",
+      "font-src 'self' https://fonts.gstatic.com",
+      "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://api.resend.com",
+      'frame-src https://www.youtube.com https://www.facebook.com',
+      "object-src 'none'",
+      "base-uri 'self'",
+      "form-action 'self'",
+    ].join('; ')
+  );
+  return response;
+};
