ca0c7d9543d33b3d64168acd69897bf550fd8d27
skailan-saas <skailan.saas@gmail.com>
2025-08-06 15:59:14 -0500

    feat: Agregar campos completos para migración a Strapi en RSS
    - Agregar campos strapi:* para migración completa a Strapi
    - Incluir isoDate para compatibilidad con n8n
    - Agregar slug, categories y campos adicionales
    - Incluir todos los metadatos necesarios para la migración
    - Campos agregados: strapi:title, strapi:slug, strapi:description, strapi:category, strapi:subcategory, strapi:tags, strapi:quote, strapi:cover, strapi:coverAlt, strapi:authorName, strapi:authorDescription, strapi:authorImage, strapi:pubDate, strapi:isoDate, strapi:content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -33,15 +33,20 @@ export async function GET() {
       // Obtener el contenido completo del artículo y escapar caracteres especiales
       const contentHtml = escapeXml(post.body || '');
 
+      // Fecha en formato ISO para n8n
+      const isoDate = new Date(post.data.pubDate).toISOString();
+
       return `
       <item>
         <title><![CDATA[${post.data.title}]]></title>
         <link>${site}/blog/${post.data.slug || post.id}</link>
         <guid>${site}/blog/${post.data.slug || post.id}</guid>
         <pubDate>${new Date(post.data.pubDate).toUTCString()}</pubDate>
+        <isoDate>${isoDate}</isoDate>
         <description><![CDATA[${post.data.description}]]></description>
         <author>${escapeXml(post.data.author?.name || 'IA Punto')}</author>
         <category>${escapeXml(categoriesString)}</category>
+        <categories>${escapeXml(categoriesString)}</categories>
         ${tagsString ? `<tags>${escapeXml(tagsString)}</tags>` : ''}
         <enclosure url="${post.data.cover}" type="image/jpeg" />
         <media:content url="${post.data.cover}" type="image/jpeg" />
@@ -51,7 +56,24 @@ export async function GET() {
         <authorImage>${post.data.author?.image || ''}</authorImage>
         <quote>${escapeXml(post.data.quote || '')}</quote>
         <date>${post.data.pubDate}</date>
+        <slug>${post.data.slug || post.id}</slug>
         <content:encoded><![CDATA[${contentHtml}]]></content:encoded>
+        <!-- Campos adicionales para migración a Strapi -->
+        <strapi:title>${escapeXml(post.data.title)}</strapi:title>
+        <strapi:slug>${post.data.slug || post.id}</strapi:slug>
+        <strapi:description>${escapeXml(post.data.description)}</strapi:description>
+        <strapi:category>${escapeXml(post.data.category)}</strapi:category>
+        <strapi:subcategory>${escapeXml(post.data.subcategory || '')}</strapi:subcategory>
+        <strapi:tags>${escapeXml(tagsString)}</strapi:tags>
+        <strapi:quote>${escapeXml(post.data.quote || '')}</strapi:quote>
+        <strapi:cover>${post.data.cover}</strapi:cover>
+        <strapi:coverAlt>${escapeXml(post.data.coverAlt || '')}</strapi:coverAlt>
+        <strapi:authorName>${escapeXml(post.data.author?.name || 'IA Punto')}</strapi:authorName>
+        <strapi:authorDescription>${escapeXml(post.data.author?.description || '')}</strapi:authorDescription>
+        <strapi:authorImage>${post.data.author?.image || ''}</strapi:authorImage>
+        <strapi:pubDate>${post.data.pubDate}</strapi:pubDate>
+        <strapi:isoDate>${isoDate}</strapi:isoDate>
+        <strapi:content><![CDATA[${contentHtml}]]></strapi:content>
       </item>
     `;
     });
@@ -59,7 +81,7 @@ export async function GET() {
   const items = await Promise.all(itemsPromises);
 
   const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+    <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:strapi="https://strapi.io/rss/">
       <channel>
         <title>Blog de IA Punto</title>
         <link>${site}/blog</link>
